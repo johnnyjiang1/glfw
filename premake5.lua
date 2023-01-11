67dc9153ba8b3bdddd06f920b74c347cb58e9e2c
johnnyjiang1 <jtc20000729@gmail.com>
2023-01-11 21:23:20 +0800

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -80,16 +80,13 @@ project "GLFW"
 
 	filter "configurations:Debug"
 		runtime "Debug"
-		buildoptions "/MDd"
 		symbols "on"
 
 	filter "configurations:Release"
 		runtime "Release"
-		buildoptions "/MD"
 		optimize "on"
 
 	filter "configurations:Dist"
 		runtime "Release"
 		optimize "on"
-		buildoptions "/MD"
         symbols "off"
